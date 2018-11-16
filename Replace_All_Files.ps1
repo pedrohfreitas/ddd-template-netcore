ed4c4492216fe7eafc32e6149641774995e7933f
Pedro Freitas <pedrofreitas@outlook.com>
2018-11-16 10:57:26 -0200

    atuliazação dos scripts

--- Replace_All_Files.ps1
+++ Replace_All_Files.ps1
@@ -1,30 +1,28 @@
-﻿$currentDirectory = (Get-Location).path
+﻿ param (
+    [string]$NameProject 
+ )
 
 $configFiles = Get-ChildItem . *.* -rec
 
 foreach ($file in $configFiles)
 {
-    (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "GEMP", "Template" } |
-    Set-Content $file.PSPath
-}
-
-foreach ($file in $configFiles)
-{
-    (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "Gemp", "Template" } |
-    Set-Content $file.PSPath
+    if ((Test-Path $_.PSPath)) {
+        (Get-Content $file.PSPath) | Foreach-Object { 
+            $_ -replace "Template", $NameProject 
+        } |   Set-Content $file.PSPath
+    }
 }
 
 Get-ChildItem -File -Recurse | % { 
-    Rename-Item -Path $_.PSPath -NewName $_.Name.replace("Gemp","Template") -Force
+    if ((Test-Path $_.PSPath)) {
+        Rename-Item -Path $_.PSPath -NewName $_.Name.replace("Template",$NameProject) -Force
+    }
 }
 
 foreach($Directory in Get-ChildItem -Path . -Recurse -Directory){
     
     if ((Test-Path $Directory)) {
       Write-Warning "$Directory absent from both locations"
+      Rename-Item $Directory -NewName $($Directory.Name.replace("Template",$NameProject))
     }
-
-    #Rename-Item $Directory -NewName $($Directory.Name.replace("Gemp","Template"))
 }
